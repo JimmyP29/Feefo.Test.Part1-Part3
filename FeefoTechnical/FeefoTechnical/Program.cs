37d66863691b32c9bd0ee24b63952eca4b6bb0bd
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-08 21:01:02 +0000

    Initial commit

---
+++ FeefoTechnical/FeefoTechnical/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeefoTechnical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
