91da18c944576fc3a4b3cba83181f4e126c0abe2
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-08 21:25:45 +0000

    Normaliser class

---
+++ FeefoTechnical/FeefoTechnical/models/Normaliser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeefoTechnical.models
+{
+    public class Normaliser
+    {
+        private IEnumerable<string> _normalisedJobTitles;
+
+        public Normaliser()
+        {
+            _normalisedJobTitles = new List<string>
+            {
+                "Architect",
+                "Software engineer",
+                "Quantity surveyor",
+                "Accountant",
+            };
+        }
+        public string Normalise(string jobTitle)
+        {
+         
+
+            return "";
+        }
+    }
+}
