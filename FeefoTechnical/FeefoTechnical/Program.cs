72f54c2f9b016076370bac1270a9e222c219e44c
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-09 19:23:06 +0000

    Made more user friendly, move int[] generation ArrayGenerator class to maintain SRP

--- FeefoTechnical/FeefoTechnical/Program.cs
+++ FeefoTechnical/FeefoTechnical/Program.cs
@@ -7,6 +7,7 @@ namespace FeefoTechnical
     {
         static void Main(string[] args)
         {
+            /* --- Problem 1 --- */
             var nonNormalisedTitle1 = "Java engineer";
             var nonNormalisedTitle2 = "C# engineer";
             var nonNormalisedTitle3 = "Accountant";
@@ -18,16 +19,22 @@ namespace FeefoTechnical
             var normalisedTitle3 = normaliser.Normalise(nonNormalisedTitle3);
             var normalisedTitle4 = normaliser.Normalise(nonNormalisedTitle4);
 
-            var arrayManipulator = new ArrayManipulator();
-            var array = arrayManipulator.GenerateHugeArray();
-
             Console.WriteLine("--- Problem 1 ---");
             Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle1, normalisedTitle1);
             Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle2, normalisedTitle2);
             Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle3, normalisedTitle3);
             Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle4, normalisedTitle4);
+            Console.WriteLine("");
 
-            Console.Write("{0}", array);
+
+
+            /* --- Problem 2 --- */
+            var arrayGenerator = new ArrayGenerator();
+            var array = arrayGenerator.GenerateHugeArray();
+            var arrayManipulator = new ArrayManipulator();
+            arrayManipulator.GetStats(array);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ FeefoTechnical/FeefoTechnical/models/ArrayGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FeefoTechnical.models
+{
+    public class ArrayGenerator
+    {
+        private Random _random;
+
+        public ArrayGenerator()
+        {
+            _random = new Random();
+        }
+        public int[] GenerateHugeArray()
+        {
+            var result = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                var number = _random.Next(100);
+                result[i] = number;
+            }
+
+            return result;
+        }
+    }
+}
