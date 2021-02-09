3d6b8e84522d97b1995ea99a1116ea6b66bde48e
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-09 18:16:42 +0000

    Unit tests

--- FeefoTechnical/FeefoTechnical.UnitTests/NormaliserTests.cs
+++ FeefoTechnical/FeefoTechnical.UnitTests/NormaliserTests.cs
@@ -12,12 +12,67 @@ namespace FeefoTechnical.UnitTests
             _normaliser = new Normaliser();
         }
 
-        [Fact(DisplayName ="GIVEN an un-normalised job title, " + 
-                           "WHEN passed into the Normaliser" +
-                           "THEN a normalised job title is returned.")]
-        public void Normalise_UnnormalisedJobTitlePassedIn_ReturnsNormalisedJobTitle()
+        [Fact(DisplayName = "GIVEN an empty string, " +
+                            "WHEN passed into the Normaliser" +
+                            "THEN an empty string is returned.")]
+        public void Normalise_EmptyStringPassedIn_ReturnsEmptyString()
         {
-            
+            var result = _normaliser.Normalise("");
+            Assert.Equal("", result);
+        }
+
+        [Fact(DisplayName = "GIVEN un-normalised job title 'Java engineer', " +
+                            "WHEN passed into the Normaliser" +
+                            "THEN normalised job title 'Software engineer' is returned.")]
+        public void Normalise_UnnormalisedJobTitleJavaEngineerPassedIn_ReturnsNormalisedJobTitle()
+        {
+            var result = _normaliser.Normalise("Java engineer");
+            Assert.Equal("Software engineer", result);
+        }
+
+        [Fact(DisplayName = "GIVEN un-normalised job title 'C# engineer', " +
+                            "WHEN passed into the Normaliser" +
+                            "THEN normalised job title 'Software engineer' is returned.")]
+        public void Normalise_UnnormalisedJobTitleCSharpEngineerPassedIn_ReturnsNormalisedJobTitle()
+        {
+            var result = _normaliser.Normalise("C# engineer");
+            Assert.Equal("Software engineer", result);
+        }
+
+        [Fact(DisplayName = "GIVEN un-normalised job title 'Accountant', " +
+                            "WHEN passed into the Normaliser" +
+                            "THEN normalised job title 'Accountant' is returned.")]
+        public void Normalise_UnnormalisedJobTitleAccountantPassedIn_ReturnsNormalisedJobTitle()
+        {
+            var result = _normaliser.Normalise("Accountant");
+            Assert.Equal("Accountant", result);
+        }
+
+        [Fact(DisplayName = "GIVEN un-normalised job title 'Chief Accountant', " +
+                            "WHEN passed into the Normaliser" +
+                            "THEN normalised job title 'Accountant' is returned.")]
+        public void Normalise_UnnormalisedJobTitleChiefAccountantPassedIn_ReturnsNormalisedJobTitle()
+        {
+            var result = _normaliser.Normalise("Chief Accountant");
+            Assert.Equal("Accountant", result);
+        }
+
+        [Fact(DisplayName = "GIVEN un-normalised job title 'Surveyor', " +
+                            "WHEN passed into the Normaliser" +
+                            "THEN normalised job title 'Quantity surveyor' is returned.")]
+        public void Normalise_UnnormalisedJobTitleSurveyorPassedIn_ReturnsNormalisedJobTitle()
+        {
+            var result = _normaliser.Normalise("Surveyor");
+            Assert.Equal("Quantity surveyor", result);
+        }
+
+        [Fact(DisplayName = "GIVEN un-normalised job title 'Junior Architect', " +
+                            "WHEN passed into the Normaliser" +
+                            "THEN normalised job title 'Architect' is returned.")]
+        public void Normalise_UnnormalisedJobTitleJuniorAcritectPassedIn_ReturnsNormalisedJobTitle()
+        {
+            var result = _normaliser.Normalise("Architect");
+            Assert.Equal("Architect", result);
         }
     }
 }
