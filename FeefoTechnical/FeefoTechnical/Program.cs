a1a72afd4a86487a8b1dfa51df0852211af64781
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-09 18:31:08 +0000

    Refactored to be more user friendly

--- FeefoTechnical/FeefoTechnical/Program.cs
+++ FeefoTechnical/FeefoTechnical/Program.cs
@@ -7,15 +7,22 @@ namespace FeefoTechnical
     {
         static void Main(string[] args)
         {
+            var nonNormalisedTitle1 = "Java engineer";
+            var nonNormalisedTitle2 = "C# engineer";
+            var nonNormalisedTitle3 = "Accountant";
+            var nonNormalisedTitle4 = "Chief Accountant";
             var normaliser = new Normaliser();
-            var result = normaliser.Normalise("Java engineer");
-            var result2 = normaliser.Normalise("C# engineer");
-            var result3 = normaliser.Normalise("Accountant");
-            var result4 = normaliser.Normalise("Chief Accountant");
-            Console.WriteLine(result);
-            Console.WriteLine(result2);
-            Console.WriteLine(result3);
-            Console.WriteLine(result4);
+
+            var normalisedTitle1 = normaliser.Normalise(nonNormalisedTitle1);
+            var normalisedTitle2 = normaliser.Normalise(nonNormalisedTitle2);
+            var normalisedTitle3 = normaliser.Normalise(nonNormalisedTitle3);
+            var normalisedTitle4 = normaliser.Normalise(nonNormalisedTitle4);
+
+            Console.WriteLine("--- Problem 1 ---");
+            Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle1, normalisedTitle1);
+            Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle2, normalisedTitle2);
+            Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle3, normalisedTitle3);
+            Console.WriteLine("Input: {0}, Output: {1}", nonNormalisedTitle4, normalisedTitle4);
         }
     }
 }
