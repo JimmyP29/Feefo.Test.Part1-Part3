eaeb3c43ebf18311bdc532c9199329c7a1c1a47b
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-09 19:23:39 +0000

    ArrayManipulatorStats class for capturing output data and printing

---
+++ FeefoTechnical/FeefoTechnical/models/ArrayManipulatorStats.cs
@@ -0,0 +1,20 @@
+﻿namespace FeefoTechnical.models
+{
+    public class ArrayManipulatorStats
+    {
+        public int Median { get; }
+        public int Mean { get; }
+        public int Mode { get; }
+        public int Range { get; }
+
+        public ArrayManipulatorStats(int median, int mean, int mode, int range)
+        {
+            Median = median;
+            Mean = mean;
+            Mode = mode;
+            Range = range;
+        }
+
+        public string Print() => $"Median: {Median}, Mean: {Mean}, Mode: {Mode}, Range: {Range}";
+    }
+}
